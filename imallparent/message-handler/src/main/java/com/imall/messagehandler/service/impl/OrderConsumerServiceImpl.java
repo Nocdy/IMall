b25f7d0ec719c08bdcb2baf8c607547e9cce289f
Nocdy <954272099@qq.com>
2022-04-16 18:21:46 +0800

    修改订单确认逻辑

--- imallparent/message-handler/src/main/java/com/imall/messagehandler/service/impl/OrderConsumerServiceImpl.java
+++ imallparent/message-handler/src/main/java/com/imall/messagehandler/service/impl/OrderConsumerServiceImpl.java
@@ -15,6 +15,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import static com.imall.constants.MessageQueueConstants.END_SELL_0_CHANNEL;
 
 /**
@@ -44,14 +46,23 @@ public class OrderConsumerServiceImpl implements OrderConsumerService {
         log.info("进行确认订单业务");
         JSONObject result = new JSONObject();
         String key=OrderServiceImpl.GENERATE_KEY.get(isFlash).apply(clientId.toString());
+        AtomicBoolean status= new AtomicBoolean(true);
         if (orderService.confirmOrder(clientId, isFlash)) {
             Order order = objectMapper.convertValue(redisUtils.get(key), Order.class);
             order.getGoodsList().forEach(goods -> {
+                redisUtils.del(goods.getId().toString());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                redisUtils.del(goods.getId().toString());
                 if (!goodsService.updatePessimistic(goods)) {
                     if (goods.getIsPlash()) {
                         iMessage.sendMessage(goods.getId().toString(), END_SELL_0_CHANNEL);
                     }
                     try {
+                        status.set(false);
                         throw new Exception("库存呢不足");
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -59,19 +70,21 @@ public class OrderConsumerServiceImpl implements OrderConsumerService {
                 }
             });
             orderService.delOrderInRedis(clientId, isFlash);
-            result.put("message", "确认订单成功!");
-            return new Result<>(result,
-                    StatusCode.SUCCESS.getCode(),
-                    StatusCode.SUCCESS.getMessage());
+            if(status.get()){
+                result.put("message", "确认订单成功!");
+            }
+            else {
+                result.put("message", "库存不足，订单确认失败!");
+            }
         } else {
             result.put("message", "你的订单已过期");
             Order rollBackOrder = orderService.getById(orderService.getExpiredOrderId(clientId));
             if (isFlash) {
                 orderService.rollBackForFlash(rollBackOrder);
             }
-            return new Result<>(result,
-                    StatusCode.SUCCESS.getCode(),
-                    StatusCode.SUCCESS.getMessage());
         }
+        return new Result<>(result,
+                StatusCode.SUCCESS.getCode(),
+                StatusCode.SUCCESS.getMessage());
     }
 }
